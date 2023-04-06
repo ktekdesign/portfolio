6cd2b5a88e8063395df123fa12e0ae6eb72318a3
ktekdesign <contact@ktekdesign.com>
2023-04-06 11:23:19 -0300

    fix: remove env var from next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
+const path = require("path")
 const nextConfig = {
-    swcMinify: true,
-    trailingSlash: true,
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-    },
-    optimizeFonts: false,
-    env: {
-      customKey: 'my-value',
-    }
-  }
+  swcMinify: true,
+  trailingSlash: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  optimizeFonts: false
+}
 
 module.exports = nextConfig
